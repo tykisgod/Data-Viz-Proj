a91668fbdc141ae0182b42b40db92f62a8af9086
TYK <tyk.helloworld@gmail.com>
2018-11-27 19:38:31 -0800

    improve

--- src/app/firstpart/firstpart.component.ts
+++ src/app/firstpart/firstpart.component.ts
@@ -179,132 +179,130 @@ export class FirstpartComponent implements OnInit {
             tooltip_first.classed("hidden", true);
           });
       });
-    });
 
-    slide_for_total_num()
-
+      slide_for_total_num()
 
 
-    function slide_for_total_num() {
 
-      // slide_for_launch_site()
+      function slide_for_total_num() {
 
-      var radius = 20;
-      var margin = 100;
+        // slide_for_launch_site()
 
-      var x1 = margin;
-      var x2 = width - margin;
-      var y = height / 2;
+        var radius = 20;
+        var margin = 100;
 
-      var drag: any = d3.drag()
-        .on("drag", dragmove);
+        var x1 = margin;
+        var x2 = width - margin;
+        var y = height / 2;
 
-      var svg_slider = d3.select("#worldmap")
-        .append("svg")
-        .attr("class", "first")
-        .attr("width", width)
-        .attr("height", 200)
-        .append("g")
-        // .attr("transform", "translate(0 -300)")
-        // .attr("width", width)
-        // .attr("height", 200)
-        .datum({
-          x: width / 2,
-          y: height / 2
-        });
+        var drag: any = d3.drag()
+          .on("drag", dragmove);
 
-      var line = svg_slider.append("line")
-        .attr("x1", x1)
-        .attr("x2", x2)
-        .attr("y1", 100)
-        .attr("y2", 100)
-        .style("stroke", "black")
-        .style("stroke-linecap", "round")
-        .style("stroke-width", 5);
+        var svg_slider = d3.select("#worldmap")
+          .append("svg")
+          .attr("class", "first")
+          .attr("width", width)
+          .attr("height", 200)
+          .append("g")
+          // .attr("transform", "translate(0 -300)")
+          // .attr("width", width)
+          // .attr("height", 200)
+          .datum({
+            x: width / 2,
+            y: height / 2
+          });
 
-      var circle = svg_slider.append("circle")
-        .attr("id", "slider_circle")
-        .attr("r", radius)
-        .attr("cy", 100)
-        .attr("cx", function (d) { return d.x; })
-        .style("cursor", "ew-resize")
-        .call(drag);
+        var line = svg_slider.append("line")
+          .attr("x1", x1)
+          .attr("x2", x2)
+          .attr("y1", 100)
+          .attr("y2", 100)
+          .style("stroke", "black")
+          .style("stroke-linecap", "round")
+          .style("stroke-width", 5);
 
-      var last_time = -1;
+        var circle = svg_slider.append("circle")
+          .attr("id", "slider_circle")
+          .attr("r", radius)
+          .attr("cy", 100)
+          .attr("cx", function (d) { return d.x; })
+          .style("cursor", "ew-resize")
+          .call(drag);
 
-      var possible_year_list = [1974, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018];
+        var last_time = -1;
 
-      var all_in_one_by_year_data = Array()
-      d3.json("src/assets/num_by_owner_data/all_in_one_by_year_data.json").then(function (json: any) {
-        all_in_one_by_year_data = json
-      })
+        var possible_year_list = [1974, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018];
 
-      var site_all_in_one_by_year_data = Array()
-      d3.json("src/assets/num_by_site_data/site_all_in_one_by_year_data.json").then(function (json: any) {
-        site_all_in_one_by_year_data = json
-      })
+        var all_in_one_by_year_data = Array()
+        d3.json("src/assets/num_by_owner_data/all_in_one_by_year_data.json").then(function (json: any) {
+          all_in_one_by_year_data = json
+        })
 
-      function get_year(d) {
-        var percent = (d - 100) / (width - 2 * margin)
-        return 1974 + Math.floor(44 * percent);
-      }
-      function dragmove(d) {
-        // var date_now = new Date();
-        // var origin_time = date_now.getTime();
-        // var now_time = date_now.getTime();
-        // console.log("start_time" + now_time)
-        // console.log(all_in_one_by_year_data)
-        var x = d3.event.x;
-        x = x < x1 ? x1 : x > x2 ? x2 : x;
-        d.x = x;
-        circle.attr("cx", x);
-        var x_value_of_circle: any = svg_slider
-          .select('#slider_circle')
-          .attr("cx")
-        x_value_of_circle = get_year(x_value_of_circle)
-        // var date_now = new Date();
-        // var original_time = date_now.getTime();
-        if (possible_year_list.includes(x_value_of_circle) && show_country_lauch) {
-          if (last_time != x_value_of_circle) {
-            d3.json("src/assets/world_geojson.json").then(function (json: any) {
-              temp_country_jsonfeatures = json.features
-              var country = all_in_one_by_year_data[possible_year_list.indexOf(x_value_of_circle)][x_value_of_circle]
-              var countryArray = new Array();
-              countryArray = country.map(d => d.country);
-              var valueArray = country.map(d => d.num);
-              temp_country_valuearray = valueArray
-              temp_country_countryArray = countryArray
+        var site_all_in_one_by_year_data = Array()
+        d3.json("src/assets/num_by_site_data/site_all_in_one_by_year_data.json").then(function (json: any) {
+          site_all_in_one_by_year_data = json
+        })
 
-              gg.selectAll(".states")
-                .data(json.features)
-                .transition()
-                .duration(150)
-                .attr("fill", function (d, i) {
-                  var index = countryArray.indexOf(d["properties"]["NAME"]);
-                  if (index >= 0) {
-                    // console.log(d["properties"]["NAME"] + " " + valueArray[index]);
-                    if (valueArray[index] == 0) {
-                      return "white"
-                    } else {
-                      return (colorScale(valueArray[index]));
-                    }
-                  } else {
-                    return "white";
-                  }
-                });
-
-
-              // .attr("stroke", "black")
-              // .attr("d", path);
-
-              // var third_count = date_now.getTime();
-              // console.log("after redraw" + third_count)
-            });
-          }
+        function get_year(d) {
+          var percent = (d - 100) / (width - 2 * margin)
+          return 1974 + Math.floor(44 * percent);
         }
-        if (possible_year_list.includes(x_value_of_circle) && show_launch_site) {
-          if (last_time != x_value_of_circle) {
-            d3.json("src/assets/world_geojson.json").then(function (json: any) {
+        function dragmove(d) {
+          // var date_now = new Date();
+          // var origin_time = date_now.getTime();
+          // var now_time = date_now.getTime();
+          // console.log("start_time" + now_time)
+          // console.log(all_in_one_by_year_data)
+          var x = d3.event.x;
+          x = x < x1 ? x1 : x > x2 ? x2 : x;
+          d.x = x;
+          circle.attr("cx", x);
+          var x_value_of_circle: any = svg_slider
+            .select('#slider_circle')
+            .attr("cx")
+          x_value_of_circle = get_year(x_value_of_circle)
+          // var date_now = new Date();
+          // var original_time = date_now.getTime();
+          if (possible_year_list.includes(x_value_of_circle) && show_country_lauch) {
+            if (last_time != x_value_of_circle) {
+              d3.json("src/assets/world_geojson.json").then(function (json: any) {
+                temp_country_jsonfeatures = json.features
+                var country = all_in_one_by_year_data[possible_year_list.indexOf(x_value_of_circle)][x_value_of_circle]
+                var countryArray = new Array();
+                countryArray = country.map(d => d.country);
+                var valueArray = country.map(d => d.num);
+                temp_country_valuearray = valueArray
+                temp_country_countryArray = countryArray
+
+                gg.selectAll(".states")
+                  .data(json.features)
+                  .transition()
+                  .duration(150)
+                  .attr("fill", function (d, i) {
+                    var index = countryArray.indexOf(d["properties"]["NAME"]);
+                    if (index >= 0) {
+                      // console.log(d["properties"]["NAME"] + " " + valueArray[index]);
+                      if (valueArray[index] == 0) {
+                        return "white"
+                      } else {
+                        return (colorScale(valueArray[index]));
+                      }
+                    } else {
+                      return "white";
+                    }
+                  });
+
+
+                // .attr("stroke", "black")
+                // .attr("d", path);
+
+                // var third_count = date_now.getTime();
+                // console.log("after redraw" + third_count)
+              });
+            }
+          }
+          if (possible_year_list.includes(x_value_of_circle) && show_launch_site) {
+            if (last_time != x_value_of_circle) {
               var projection = d3.geoMercator().fitSize([width, height * 1.4], json);
               d3.json("src/assets/num_by_site_data/site_long_lat_data.json").then(function (site_long_lat: any) {
                 var site_num_data = site_all_in_one_by_year_data[possible_year_list.indexOf(x_value_of_circle)][x_value_of_circle]
@@ -356,110 +354,108 @@ export class FirstpartComponent implements OnInit {
                     tooltip_first.classed("hidden", true);
                   })
               });
-            });
+            }
           }
+          last_time = x_value_of_circle
+          // var timecosume = date_now.getTime() - origin_time;
+          // console.log("update use " + timecosume + " ms")
         }
-        last_time = x_value_of_circle
-        // var timecosume = date_now.getTime() - origin_time;
-        // console.log("update use " + timecosume + " ms")
       }
-    }
 
-    //deal with checkbox
-    d3.select("#by_launch_site").on("change", update_by_launch_site);
-    d3.select("#by_country").on("change", update_by_country);
+      //deal with checkbox
+      d3.select("#by_launch_site").on("change", update_by_launch_site);
+      d3.select("#by_country").on("change", update_by_country);
 
-    function update_by_launch_site() {
-      if (d3.select("#by_launch_site").property("checked")) {
-        d3.json("src/assets/world_geojson.json").then(function (json: any) {
-          var projection = d3.geoMercator().fitSize([width, height * 1.4], json);
-          console.log("by_launch_site: true")
-          show_launch_site = true;
-          svg.selectAll("circle")
-            .data(temp_site_data)
+      function update_by_launch_site() {
+        if (d3.select("#by_launch_site").property("checked")) {
+            var projection = d3.geoMercator().fitSize([width, height * 1.4], json);
+            console.log("by_launch_site: true")
+            show_launch_site = true;
+            svg.selectAll("circle")
+              .data(temp_site_data)
+              .enter()
+              .append("circle")
+              .attr("class", "site_circle")
+              .attr("cx", function (d: any, i) {
+                return projection(d[2])[0]
+              }
+              )
+              .attr("cy", function (d: any, i) {
+                return projection(d[2])[1]
+              }
+              )
+              .attr("r", function (d: any, i) {
+                return adapt_site_data(d[1])
+              }
+              )
+              .attr("fill", "blue")
+              .attr("opacity", "0.7")
+              .on("mouseover", function (d: any, i) {
+                return tooltip_first.style("hidden", false).html("<p>Site: " + d[0] + '</p><p>' + 'Num of Launch:' + d[1] + "</p>");
+              })
+              .on("mousemove", function (d: any) {
+                tooltip_first.classed("hidden", false)
+                  .style("top", (d3.event.pageY) + "px")
+                  .style("left", (d3.event.pageX + 10) + "px")
+                  .html("<p>Site: " + d[0] + '</p><p>' + 'Num of Launch:' + d[1] + "</p>");
+              })
+              .on("mouseout", function (d: any, i) {
+                tooltip_first.classed("hidden", true);
+              })
+        } else {
+          console.log("by_launch_site: false")
+          show_launch_site = false;
+          svg.selectAll(".site_circle").remove()
+        }
+      }
+
+      function update_by_country() {
+        if (d3.select("#by_country").property("checked")) {
+          console.log("show_country_lauch: true")
+          show_country_lauch = true;
+          gg.selectAll(".states")
+            .data(temp_country_jsonfeatures)
             .enter()
-            .append("circle")
-            .attr("class", "site_circle")
-            .attr("cx", function (d: any, i) {
-              return projection(d[2])[0]
-            }
-            )
-            .attr("cy", function (d: any, i) {
-              return projection(d[2])[1]
-            }
-            )
-            .attr("r", function (d: any, i) {
-              return adapt_site_data(d[1])
-            }
-            )
-            .attr("fill", "blue")
-            .attr("opacity", "0.7")
+            .append("path")
+            .attr("class", "states")
+            .attr("fill", function (d, i) {
+              var index = temp_country_countryArray.indexOf(d["properties"]["NAME"]);
+              if (index >= 0) {
+                // console.log(d["properties"]["NAME"] + " " + valueArray[index]);
+                if (temp_country_valuearray[index] == 0) {
+                  return "white"
+                } else {
+                  return (colorScale(temp_country_valuearray[index]));
+                }
+              } else {
+                return "white";
+              }
+            })
+            .attr("stroke", "black")
+            .attr("d", path)
             .on("mouseover", function (d: any, i) {
-              return tooltip_first.style("hidden", false).html("<p>Site: " + d[0] + '</p><p>' + 'Num of Launch:' + d[1] + "</p>");
+              d3.select(this).attr("stroke-width", 2);
+              return tooltip_first.style("hidden", false).html("<p>Country: " + d["properties"]["NAME"] + '</p><p>' + 'Num of Launch:' + temp_country_valuearray[temp_country_countryArray.indexOf(d["properties"]["NAME"])] + "</p>");
             })
             .on("mousemove", function (d: any) {
               tooltip_first.classed("hidden", false)
                 .style("top", (d3.event.pageY) + "px")
                 .style("left", (d3.event.pageX + 10) + "px")
-                .html("<p>Site: " + d[0] + '</p><p>' + 'Num of Launch:' + d[1] + "</p>");
+                .html("<p>Country: " + d["properties"]["NAME"] + '</p><p>' + 'Num of Launch:' + temp_country_valuearray[temp_country_countryArray.indexOf(d["properties"]["NAME"])] + "</p>");
             })
             .on("mouseout", function (d: any, i) {
+              d3.select(this).attr("stroke-width", 1);
               tooltip_first.classed("hidden", true);
             })
-        })
-      } else {
-        console.log("by_launch_site: false")
-        show_launch_site = false;
-        svg.selectAll(".site_circle").remove()
-      }
-    }
 
-    function update_by_country() {
-      if (d3.select("#by_country").property("checked")) {
-        console.log("show_country_lauch: true")
-        show_country_lauch = true;
-        gg.selectAll(".states")
-          .data(temp_country_jsonfeatures)
-          .enter()
-          .append("path")
-          .attr("class", "states")
-          .attr("fill", function (d, i) {
-            var index = temp_country_countryArray.indexOf(d["properties"]["NAME"]);
-            if (index >= 0) {
-              // console.log(d["properties"]["NAME"] + " " + valueArray[index]);
-              if (temp_country_valuearray[index] == 0) {
-                return "white"
-              } else {
-                return (colorScale(temp_country_valuearray[index]));
-              }
-            } else {
-              return "white";
-            }
-          })
-          .attr("stroke", "black")
-          .attr("d", path)
-          .on("mouseover", function (d: any, i) {
-            d3.select(this).attr("stroke-width", 2);
-            return tooltip_first.style("hidden", false).html("<p>Country: " + d["properties"]["NAME"] + '</p><p>' + 'Num of Launch:' + temp_country_valuearray[temp_country_countryArray.indexOf(d["properties"]["NAME"])] + "</p>");
-          })
-          .on("mousemove", function (d: any) {
-            tooltip_first.classed("hidden", false)
-              .style("top", (d3.event.pageY) + "px")
-              .style("left", (d3.event.pageX + 10) + "px")
-              .html("<p>Country: " + d["properties"]["NAME"] + '</p><p>' + 'Num of Launch:' + temp_country_valuearray[temp_country_countryArray.indexOf(d["properties"]["NAME"])] + "</p>");
-          })
-          .on("mouseout", function (d: any, i) {
-            d3.select(this).attr("stroke-width", 1);
-            tooltip_first.classed("hidden", true);
-          })
-
-      } else {
-        console.log("show_country_lauch: false")
-        gg.selectAll(".states").remove()
-        // gg.selectAll(".states")
-        //   .attr("fill", "white")
-        show_country_lauch = false;
+        } else {
+          console.log("show_country_lauch: false")
+          gg.selectAll(".states").remove()
+          // gg.selectAll(".states")
+          //   .attr("fill", "white")
+          show_country_lauch = false;
+        }
       }
-    }
+    });
   }
 }
